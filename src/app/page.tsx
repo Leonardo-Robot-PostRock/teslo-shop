fb539c540d482917fea6c60cd5d86614bb5e9138
Leonardo <leonardo38361@gmail.com>
2024-09-11 00:03:38 -0300

    uso de fuentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { titleFont } from '../config/fonts';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hola mundo</h1>
+      <h1>titleFont.class</h1>
     </main>
   );
 }
