1f59c89422fe3e920a5df33261faa144ff1b0bf2
Leonardo <leonardo38361@gmail.com>
2024-09-11 00:58:02 -0300

    estructura de aplicación

---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { titleFont } from '@/config/fonts';
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>New Account</h1>
+    </main>
+  );
+}
